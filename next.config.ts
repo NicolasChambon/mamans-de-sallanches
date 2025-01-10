a1a934f2e9da8de44f83df511f88ec397f22a525
NicolasChambon <nico.chambon@gmail.com>
2025-01-10 15:19:27 +0100

    project setup done

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import path from "path"; // we need to import path from Node.js to access .join method
 
 const nextConfig: NextConfig = {
-  /* config options here */
+
+  // The includePaths option allows us to specify the directories where Sass should look for files to import
+  sassOptions: {
+    includePaths: [path.join(__dirname, "src/styles")],
+  }
 };
 
 export default nextConfig;
---
+++ src/assets/fonts.ts
@@ -0,0 +1,16 @@
+// Font importation and configuration for the project
+// Definition of the fonts to be used as css variables in all css and scss files
+
+import { Inter, Poppins } from 'next/font/google';
+
+export const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+});
+
+export const poppins = Poppins({
+  subsets: ['latin'],
+  variable: '--font-poppins',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+});
