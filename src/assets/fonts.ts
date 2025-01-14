8479461305946944a7ea49aa71cfbcc872c5a169
NicolasChambon <nico.chambon@gmail.com>
2025-01-14 14:10:41 +0100

    logo integration to the project

--- src/assets/fonts.ts
+++ src/assets/fonts.ts
@@ -1,7 +1,7 @@
 // Font importation and configuration for the project
 // Definition of the fonts to be used as css variables in all css and scss files
 
-import { Inter, Poppins } from 'next/font/google';
+import { Inter, Poppins, Dancing_Script } from 'next/font/google';
 
 export const inter = Inter({
   subsets: ['latin'],
@@ -13,4 +13,10 @@ export const poppins = Poppins({
   subsets: ['latin'],
   variable: '--font-poppins',
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+});
+
+export const dancingScript = Dancing_Script({
+  subsets: ['latin'],
+  variable: '--font-dancing-script',
+  weight: ['400', '500', '600', '700']
 });
