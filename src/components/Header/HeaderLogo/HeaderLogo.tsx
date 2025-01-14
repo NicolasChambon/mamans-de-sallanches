f7e145a13c2a5439c053a083f773fc89b81ecfa6
NicolasChambon <nico.chambon@gmail.com>
2025-01-14 15:16:16 +0100

    header logo done

--- src/components/Header/HeaderLogo/HeaderLogo.tsx
+++ src/components/Header/HeaderLogo/HeaderLogo.tsx
@@ -1,7 +1,21 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+import MainLogo from '@/assets/logos/logo-mamans-de-sallanches.svg';
+
 import styles from './HeaderLogo.module.scss';
 
 const HeaderLogo = () => {
-  return <div className={styles.HeaderLogo}>HeaderLogo</div>;
+  return (
+    <Link className={styles.headerLogo} href="/">
+      <Image
+        className={styles.headerLogo_img}
+        src={MainLogo}
+        alt="Logo Mamans de Sallanches"
+      />
+      <p className={styles.headerLogo_text}>Mamans de Sallanches</p>
+    </Link>
+  );
 };
 
 export default HeaderLogo;
